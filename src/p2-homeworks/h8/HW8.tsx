497d705fa86dbc1d3b8a0f4274b27a09609a2693
ShuVeriDA <bashtarov@outlook.com>
2022-05-01 13:39:11 +0300

    HW8

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -2,47 +2,62 @@ import React, {useState} from 'react'
 import {homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
-// export type UserType =
+export type UserType = {
+   _id: number,
+   name: string,
+   age: number
+}
 
-const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+const initialPeople: UserType[] = [
+   {_id: 0, name: 'Кот', age: 3},
+   {_id: 1, name: 'Александр', age: 66},
+   {_id: 2, name: 'Коля', age: 16},
+   {_id: 3, name: 'Виктор', age: 44},
+   {_id: 4, name: 'Дмитрий', age: 40},
+   {_id: 5, name: 'Ирина', age: 55},
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+   const [people, setPeople] = useState<UserType[]>(initialPeople) // need to fix any
 
-    // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
-        </div>
-    ))
+   // need to fix any
+   const finalPeople = people.map((p: UserType) => (
+      <div key={p._id} style={{display: 'flex', justifyContent: 'space-between', width: '220px'}}>
+         <span>{p.name}</span>
+         <span>{p.age}</span>
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+      </div>
+   ))
 
-    return (
-        <div>
-            <hr/>
-            homeworks 8
+   const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+   const sortDown = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'}))
+   const check = () => setPeople(homeWorkReducer(initialPeople, {type: 'check', payload: 'check18'}))
 
-            {/*should work (должно работать)*/}
+   return (
+      <div>
+         <hr/>
+         homeworks 8
+
+         {/*should work (должно работать)*/}
+         <div>
             {finalPeople}
+         </div>
 
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativePeople/>*/}
-            <hr/>
-        </div>
-    )
+
+         <div style={{display: 'flex', justifyContent: 'space-between', width: '220px'}}>
+            <SuperButton onClick={sortUp}>sort up</SuperButton>
+            <SuperButton onClick={sortDown}>sort down</SuperButton>
+            <SuperButton onClick={check}>check 18</SuperButton>
+         </div>
+
+
+         <hr/>
+         {/*для личного творчества, могу проверить*/}
+         {/*<AlternativePeople/>*/}
+         <hr/>
+      </div>
+   )
 }
 
 export default HW8
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,23 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType = {
+    type: string,
+    [key: string]: string | number
+}
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
+            const newSort = [...state]
+            const newState = newSort.sort((a: UserType , b:UserType ) => a.name < b.name ? -1 : 1)
+            return action.payload === 'up' ? newState : newState.reverse()
+
         }
         case 'check': {
             // need to fix
-            return state
+            const newSort = [...state]
+            return newSort.filter(f => f.age >= 18 )
         }
         default: return state
     }
