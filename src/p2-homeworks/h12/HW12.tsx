3949ae7f9183ed9a89fee27ae2d72e5be3a99483
ShuVeriDA <bashtarov@outlook.com>
2022-06-14 20:17:07 +0300

    HW12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import s from "./HW12.module.css";
-
-const themes = ['dark', 'red', 'some'];
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../h10/bll/store";
+import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
+import {changeThemeC} from "./bll/themeReducer";
 
 function HW12() {
-    const theme = 'some'; // useSelector
+   const themes = useSelector<AppStoreType, string[]>(state => state.themes.themes)
+   const theme = useSelector<AppStoreType, string>(state => state.themes.value)
+   const dispatch = useDispatch()
 
-    // useDispatch, onChangeCallback
+   const [value, setValue] = useState<string>(theme)
+   const onChangeCallback = (theme: string) => {
+      dispatch(changeThemeC(theme))
+      setValue(theme)
+   }
+
+   const bgColor = value === 'dark' ? s.dark : '' || value === 'red' ? s.red : '' || value === 'some' ? s.some: ""
+   const color = value === 'dark' ? s.darkText : '' || value === 'red' ? s.redText : '' || value === 'some' ? s.someText : ""
 
     return (
-        <div className={s[theme]}>
+        <div className={bgColor}>
             <hr/>
-            <span className={s[theme + '-text']}>
+            <span className={color}>
                 homeworks 12
             </span>
+            <div className={color}>
+               <h3>Option: {theme}</h3>
+               <SuperRadio
+                  className={bgColor}
+                  name={'radio'}
+                  options={themes}
+                  value={value}
+                  onChangeOption={onChangeCallback}
+               />
+            </div>
 
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
 
             <hr/>
         </div>
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,31 @@
-const initState = {
+export const CHANGE_THEME = "CHANGE_THEME"
 
+type initStateType = {
+    themes: string[]
+    value: string
+}
+
+const initState = {
+    themes: ['dark', 'red', 'some'],
+    value: 'red'
 };
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state: initStateType = initState, action: ActionType): initStateType => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
-        }
+        case CHANGE_THEME:
+            return {
+                ...state,
+                value: action.theme
+            }
         default: return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+export type ActionType = ReturnType<typeof changeThemeC>
+
+export const changeThemeC = (theme: string) => {
+   return {
+       type: CHANGE_THEME,
+       theme
+   } as const
+};
